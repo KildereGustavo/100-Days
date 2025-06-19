5f994f2bd7897572dde8fbc97096a518c73edaa7
Kildere Gustavo Alves dos Santos <kildere2029@gmail.com>
2025-06-18 22:33:12 -0300

    final program

--- Days/Day1/Code.py
+++ Days/Day1/Code.py
@@ -1,3 +1,5 @@
+#Ideia do programa é fazer um gerador de nome, onde voce recebe a entrada do nome da cidade onde o usuário cresceu, e o nome de seu pet, concatenando esses dois inputs
+
 print('Welcome to the Band Name Generator.')
 name_city = str(input('What'"'"'s the name of the city you grew up in?\n'))
 name_pet = str(input('What'"'"'s your pet'"'s ?\n"))
