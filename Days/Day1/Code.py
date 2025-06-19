d2eec5c23ed5374c627633b596582a5125afee8a
Kildere Gustavo Alves dos Santos <kildere2029@gmail.com>
2025-06-18 21:54:31 -0300

    teste 2

--- Days/Day1/Code.py
+++ Days/Day1/Code.py
@@ -1,4 +1,4 @@
 print('Welcome to the Band Name Generator.')
 name_city = str(input('What'"'"'s the name of the city you grew up in?\n'))
-name_pet = str(input('What'"'"'s your pet'"'s name?\n"))
+name_pet = str(input('What'"'"'s your pet'"'s ?\n"))
 print('Your band name could be '+name_city+' '+name_pet)
