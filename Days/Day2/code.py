79ae1b6092c93c5a03ff49f3a722c4dd35ddebc7
Kildere Gustavo Alves dos Santos <kildere2029@gmail.com>
2025-06-19 21:30:41 -0300

    Create code.py

---
+++ Days/Day2/code.py
@@ -0,0 +1,8 @@
+# A ideia é fazer um programa que calcule a conta que cada um deve pagar em uma conta de restaurante
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+print(f'Each person should pay: {round((bill+bill*(tip/100))/people, 2)}')
