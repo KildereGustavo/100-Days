bfb8c016ce2e15687d726effe6d2dc0d40ecc4f9
Kildere Gustavo Alves dos Santos <kildere2029@gmail.com>
2025-06-19 21:31:46 -0300

    Update code.py

--- Days/Day2/code.py
+++ Days/Day2/code.py
@@ -5,4 +5,5 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+# Então vamos fazer o valor total, ou seja, conta + gorjeta e depois dividir pelo numéro de pessoas..
 print(f'Each person should pay: {round((bill+bill*(tip/100))/people, 2)}')
